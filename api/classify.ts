48fb4faaee425f949edbceaa84d7276d9fce7d27
ahamedafri <ahamedafricontact@gmail.com>
2026-07-05 09:26:34 +0530

    commit 3

--- api/classify.ts
+++ api/classify.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import dotenv from "dotenv";
-import { classifyMessageWithGroq, heuristicClassify } from "../src/services/classifier";
+import { classifyMessageWithGroq, heuristicClassify } from "./classifier_utils";
 
 dotenv.config();
 
--- api/draft-reply.ts
+++ api/draft-reply.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import dotenv from "dotenv";
-import { draftReplyWithGroq, heuristicDraftReply } from "../src/services/classifier";
+import { draftReplyWithGroq, heuristicDraftReply } from "./classifier_utils";
 
 dotenv.config();
 
